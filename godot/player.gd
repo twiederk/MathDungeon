221cc66d3914387e4714faa5854e665f20107791
IN57KC <torsten.wiederkehr@ing.com>
2025-12-24 04:47:49 +0100

    Created player and zombie scene

---
+++ godot/player.gd
@@ -0,0 +1,10 @@
+class_name Player
+extends CharacterBody2D
+
+const SPEED: float = 200.0
+
+func _physics_process(delta: float) -> void:
+	# Holt einen normierten Bewegungsvektor aus den UI-Inputs
+	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = dir * SPEED
+	move_and_slide()
