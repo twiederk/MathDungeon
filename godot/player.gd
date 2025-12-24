cb0c39a76c3e3e89333d84a78ffde7782bd207bf
IN57KC <torsten.wiederkehr@ing.com>
2025-12-24 05:23:03 +0100

    Removed debugger warning

--- godot/player.gd
+++ godot/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED: float = 200.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Holt einen normierten Bewegungsvektor aus den UI-Inputs
 	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = dir * SPEED
