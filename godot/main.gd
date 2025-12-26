16e9652d7868d020ea06e212445b43635472f2ca
IN57KC <torsten.wiederkehr@ing.com>
2025-12-26 13:27:29 +0100

    Added sword

--- godot/main.gd
+++ godot/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var quiz: Control = $UI/QuizDialog
 @onready var enemies_root: Node = $Enemies
+@onready var items_root: Node = $Items
 
 
 func _init() ->  void:
@@ -17,6 +18,14 @@ func _ready() -> void:
 		if child.has_signal("encountered"):
 			child.encountered.connect(_on_enemy_encountered)
 
+	for child in items_root.get_children():
+		if child.has_signal("item_picked_up"):
+			child.item_picked_up.connect(_on_item_picked_up)
+
 
 func _on_enemy_encountered(enemy: Area2D) -> void:
 	quiz.open_for(enemy)
+
+
+func _on_item_picked_up(item: Item) -> void:
+	item.queue_free()
