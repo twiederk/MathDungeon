81926c93bdfdd839983290ad157dd76f7c69bb86
IN57KC <torsten.wiederkehr@ing.com>
2025-12-29 06:28:28 +0100

    Added SaveManager

--- godot/gui/start_menu.gd
+++ godot/gui/start_menu.gd
@@ -1,9 +1,22 @@
 class_name StartMenu
 extends Control
 
+@onready var start_button = $CenterContainer/VBoxContainer/StartButton
+@onready var load_button = $CenterContainer/VBoxContainer/LoadButton
+
+
+func _ready():
+	if not SaveManager.is_save_game_available():
+		load_button.set_disabled(true)
+	start_button.grab_focus()
+
 
 func _on_start_game_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_load_button_pressed():
+	SaveManager.load_game()
 
 
 func _on_quit_button_pressed():
---
+++ godot/save_manager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+const TEST_PATH: String = "res://save.txt"
+const PROD_PATH: String = "user://math_dungeon_save.save"
+
+var save_path: String = PROD_PATH
+
+
+func save_game():
+	#MainInstances.stash_stats()
+	PlayerStats.stash_stats()
+	
+	var save_file = FileAccess.open(save_path, FileAccess.WRITE)
+	#var data_string = JSON.stringify(WorldStash.data)
+	#save_file.store_string(data_string)
+	save_file.close()
+
+
+func is_save_game_available():
+	return FileAccess.file_exists(save_path)
+
+
+func load_game():
+	var load_file = FileAccess.open(save_path, FileAccess.READ)
+	if not load_file is FileAccess:
+		PlayerStats.refill()
+		return null
+	
+	var data =  JSON.parse_string(load_file.get_line())
+	#WorldStash.data = data
+	
+	#var file_path = WorldStash.retrieve("level", "file_path")
+	#MainInstances.world.load_level(file_path)
+	
+	#MainInstances.retrieve_stats()
+	PlayerStats.retrieve_stats()
+	load_file.close()
