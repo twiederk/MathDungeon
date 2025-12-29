15df9f71cc477b27fc9c28860b1116943eb98a6b
IN57KC <torsten.wiederkehr@ing.com>
2025-12-29 06:45:29 +0100

    Save game always when player stats change

--- godot/player_stats.gd
+++ godot/player_stats.gd
@@ -1,4 +1,13 @@
 extends Node
 
-var damage: int = 1
-var hit_points: int = 5
+
+var damage: int = 1:
+	set(value):
+		damage = value
+		SaveManager.save_game()
+
+
+var hit_points: int = 5:
+	set(value):
+		hit_points = value
+		SaveManager.save_game()
