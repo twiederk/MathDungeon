0cd938a3b839cad776d9822abe91c8c37024a9e8
IN57KC <torsten.wiederkehr@ing.com>
2026-01-01 14:27:33 +0100

    Use German instead of English

--- godot/gui/stats_sheet.gd
+++ godot/gui/stats_sheet.gd
@@ -10,15 +10,15 @@ extends Control
 func update_player_stats() -> void:
 	health_meter_widget.update_health_ui(PlayerStats.hit_points)
 	health_meter_widget.update_max_health_ui(PlayerStats.max_hit_points)
-	damage_label.text = "Damage: " + str(PlayerStats.weapon_damage)
-	armor_label.text = "Armor: " + str(PlayerStats.armor)
+	damage_label.text = "Schaden: " + str(PlayerStats.weapon_damage)
+	armor_label.text = "Rüstung: " + str(PlayerStats.armor)
 
 
 func update_enemy_stats(enemy: Enemy) -> void:
 	health_meter_widget.update_health_ui(enemy.hit_points)
 	health_meter_widget.update_max_health_ui(enemy.stats.max_hit_points)
-	damage_label.text = "Damage: " + str(enemy.stats.damage)
-	armor_label.text = "Armor: " + str(enemy.stats.armor)
+	damage_label.text = "Schaden: " + str(enemy.stats.damage)
+	armor_label.text = "Rüstung: " + str(enemy.stats.armor)
 
 
 func update_health_only(hit_points: int) -> void:
