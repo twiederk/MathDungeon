5a2d7971905335e75df9a25c88618e8863e19a1a
IN57KC <torsten.wiederkehr@ing.com>
2026-01-01 14:33:09 +0100

    Replaced enemy_stats_update with update_stats

--- godot/gui/stats_sheet.gd
+++ godot/gui/stats_sheet.gd
@@ -13,12 +13,5 @@ func update_stats(hit_points: int, damage: int, armor: int) -> void:
 	armor_label.text = "Rüstung: " + str(armor)
 
 
-func update_enemy_stats(enemy: Enemy) -> void:
-	health_meter_widget.update_health_ui(enemy.hit_points)
-	health_meter_widget.update_max_health_ui(enemy.stats.max_hit_points)
-	damage_label.text = "Schaden: " + str(enemy.stats.damage)
-	armor_label.text = "Rüstung: " + str(enemy.stats.armor)
-
-
 func update_health_only(hit_points: int) -> void:
 	health_meter_widget.update_health_ui(hit_points)
