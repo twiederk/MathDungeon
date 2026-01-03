2c677919739d15884d4a9c6c07d8fce716b2f264
IN57KC <torsten.wiederkehr@ing.com>
2026-01-03 19:54:03 +0100

    Refactored Main._ready()

--- godot/main.gd
+++ godot/main.gd
@@ -18,21 +18,8 @@ func _init() ->  void:
 
 func _ready() -> void:
 	_setup_signals()
-		
-	var tile_map_used_rect = tile_map_layer.get_used_rect()
-	var tile_size = tile_map_layer.tile_set.tile_size
-	var north_limit = tile_map_used_rect.position.y * tile_size.y
-	var south_limit = (tile_map_used_rect.position.y + tile_map_used_rect.size.y) * tile_size.y
-	var west_limit = tile_map_used_rect.position.x * tile_size.x
-	var east_limit = (tile_map_used_rect.position.x + tile_map_used_rect.size.x) * tile_size.x
-	
-	map_borders.configure_borders(north_limit, south_limit, west_limit, east_limit)
-	player.set_camera_limits(north_limit, south_limit, west_limit, east_limit)
-
-	PlayerStats.health_changed.connect(_on_player_stats_changed)
-	PlayerStats.weapon_damage_changed.connect(_on_player_stats_changed)
-	PlayerStats.armor_changed.connect(_on_player_stats_changed)
-	player_stats_sheet.update_stats(PlayerStats.hit_points, PlayerStats.max_hit_points, PlayerStats.get_total_damage(), PlayerStats.armor)
+	_setup_limits_and_borders()
+	_setup_character_stats()
 
 
 func _setup_signals() -> void:
@@ -47,6 +34,27 @@ func _setup_signals() -> void:
 	for child in companions_root.get_children():
 		if child.has_signal("companion_picked_up"):
 			child.companion_picked_up.connect(_on_companion_picked_up)
+
+
+func _setup_limits_and_borders() -> void:
+	var tile_map_used_rect = tile_map_layer.get_used_rect()
+	var tile_size = tile_map_layer.tile_set.tile_size
+	var north_limit = tile_map_used_rect.position.y * tile_size.y
+	var south_limit = (tile_map_used_rect.position.y + tile_map_used_rect.size.y) * tile_size.y
+	var west_limit = tile_map_used_rect.position.x * tile_size.x
+	var east_limit = (tile_map_used_rect.position.x + tile_map_used_rect.size.x) * tile_size.x
+	
+	map_borders.configure_borders(north_limit, south_limit, west_limit, east_limit)
+	player.set_camera_limits(north_limit, south_limit, west_limit, east_limit)
+
+
+func _setup_character_stats() -> void:
+	PlayerStats.health_changed.connect(_on_player_stats_changed)
+	PlayerStats.weapon_damage_changed.connect(_on_player_stats_changed)
+	PlayerStats.armor_changed.connect(_on_player_stats_changed)
+	player_stats_sheet.update_stats(PlayerStats.hit_points, PlayerStats.max_hit_points, PlayerStats.get_total_damage(), PlayerStats.armor)
+
+
 func _on_enemy_encountered(enemy: StaticBody2D) -> void:
 	quiz.open_for(enemy)
 
