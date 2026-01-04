dcc6a555f2fcdf218b87b752f07a9a0aafabfda3
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-01-04 09:43:22 +0100

    Version 1.0.0
    Add nether portal to nether to return to main level

--- godot/items/nether_portal.gd
+++ godot/items/nether_portal.gd
@@ -2,5 +2,8 @@ class_name NetherPortal
 extends Item
 
 
+@export var dungeon_path: String
+
+
 func execute() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://nether.tscn")
+	get_tree().call_deferred("change_scene_to_file", dungeon_path)
