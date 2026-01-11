407cf6a0cd80e1820f55c456ac0d343f038e8312
IN57KC <torsten.wiederkehr@ing.com>
2026-01-11 16:42:10 +0100

    Created DigitSumExerciseGenerator

--- godot/test/test_DigitSumExerciseGenerator.gd
+++ godot/test/test_DigitSumExerciseGenerator.gd
@@ -1,12 +1,24 @@
 extends GutTest
 
+var digit_sum_exercise_generator: DigitSumExerciseGenerator = null
+
 
 func before_each():
-	pass
+	digit_sum_exercise_generator = DigitSumExerciseGenerator.new()
+
 
 func after_each():
-	pass
+	digit_sum_exercise_generator = null
+
+
+func test_create_exercise():
+	# arrange
+	seed(1)
 	
+	# act
+	var exercise = digit_sum_exercise_generator.create_exercise()
 	
-func test_simple():
-	assert_true(true)
+	# assert
+	assert_not_null(exercise, "Exercise should not be null")
+	assert_eq(exercise.question, "Was ist die Quersumme von 568?")
+	assert_eq(exercise.result, "19")
