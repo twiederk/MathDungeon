459220cebdfd309edb1b9749adcb6e77059151d3
IN57KC <torsten.wiederkehr@ing.com>
2026-01-13 06:14:04 +0100

    Display dungeon title when entering dungeon 1

--- godot/dungeon_title.gd
+++ godot/dungeon_title.gd
@@ -4,24 +4,24 @@ extends Node2D
 @export var title_text: String = "Dungeon Title"
 
 @onready var animation_player = $TitleDisplay/AnimationPlayer
-@onready var label = $TitleDisplay/Label
-@onready var area_2d = $Area2D
+@onready var title_label = $TitleDisplay/TitleLabel
+@onready var trigger_area_2d = $TriggerArea2D
 
 var animation_triggered = false
 
 
-func _ready():
-	label.text = title_text
+func _ready() -> void:
+	title_label.text = title_text
 	animation_player.animation_finished.connect(_on_animation_finished)
 
 
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("player") and not animation_triggered:
+func _on_area_2d_body_entered(body) -> void:
+	if body.name == "Player" and not animation_triggered:
+		title_label.visible = true
 		animation_triggered = true
 		animation_player.play("fade_out")
-		# Disable the area after triggering
-		area_2d.set_deferred("monitoring", false)
+		trigger_area_2d.set_deferred("monitoring", false)
 
 
-func _on_animation_finished():
+func _on_animation_finished() -> void:
 	queue_free()
