770af4e147742ff8b5a2a2021b51308ee1fa8cef
IN57KC <torsten.wiederkehr@ing.com>
2026-01-25 06:20:28 +0100

    Set number of required eyes of ender to 12

--- godot/locations/end_portal.gd
+++ godot/locations/end_portal.gd
@@ -1,6 +1,7 @@
 class_name EndPortal
 extends Node2D
 
+const REQUIRED_EYES_OF_ENDER: int = 12
 
 @export var dungeon_path: String
 
@@ -12,14 +13,15 @@ var active: bool = false
 
 
 func _on_outer_area_2d_body_entered(body: Node2D) -> void:
-	if body.name != "Player": return
-	
-	if PlayerStats.eyes_of_ender >= 9:
+	if body.name != "Player":
+		return
+	if PlayerStats.eyes_of_ender >= REQUIRED_EYES_OF_ENDER:
 		active = true
 		inner_sprite_2d.visible = true
 
 
 func _on_inner_area_2d_body_entered(body: Node2D) -> void:
-	if body.name != "Player": return
+	if body.name != "Player":
+		return
 	if active:
 		get_tree().call_deferred("change_scene_to_file", dungeon_path)
