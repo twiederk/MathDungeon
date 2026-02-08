f2a65476826888f2d4b6b705edd1c8c5819cc697
IN57KC <torsten.wiederkehr@ing.com>
2026-02-08 14:37:23 +0100

    Format number of digit sum question

--- godot/quiz/digit_sum_exercise_generator.gd
+++ godot/quiz/digit_sum_exercise_generator.gd
@@ -1,11 +1,14 @@
 class_name DigitSumExerciseGenerator
 
 
+var number_format = NumberFormat.new()
+
+
 func create_exercise(max_number: int = 5) -> Exercise:
 	var number_of_digits = randi_range(3, max_number)
 	var number = _generate_number_with_digits(number_of_digits)
 	var digit_sum = _calculate_digit_sum(number)
-	var question = "Was ist die Quersumme von %s?" % str(number)
+	var question = "Was ist die Quersumme von %s?" % number_format.format(number)
 	return Exercise.new(question, str(digit_sum))
 
 
