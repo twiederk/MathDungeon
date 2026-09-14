a74685edabc36bb2b1510822b8066b88d1fe6e77
in57kc <torsten.wiederkehr@ing.com>
2026-09-14 15:57:43 +0200

    Created proc_gen_world

---
+++ godot/procedural/pro_gen_world.gd
@@ -0,0 +1,24 @@
+class_name ProcGenWorld
+extends Node2D
+
+var root_node: Branch
+var tile_size: int =  16
+
+
+func _ready() -> void:
+	root_node  = Branch.new(Vector2i(0, 0), Vector2i(10, 5))
+	queue_redraw()
+
+
+func _draw() -> void:
+	for leaf in root_node.get_leaves():
+		draw_rect(
+			Rect2(
+				leaf.position.x * tile_size,
+				leaf.position.y * tile_size,
+				leaf.size.x * tile_size,
+				leaf.size.y * tile_size
+			), 
+			Color.GREEN,
+			false
+		)
