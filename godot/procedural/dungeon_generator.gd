a198e715f8060a1e674acf28c76594a1227e2862
in57kc <torsten.wiederkehr@ing.com>
2026-09-15 04:19:46 +0200

    Added DungeonGenerator

---
+++ godot/procedural/dungeon_generator.gd
@@ -0,0 +1,31 @@
+class_name DungeonGenerator
+
+
+func generate_dungeon() -> Array[Vector2i]:
+	var root_node  = Branch.new(Vector2i(0, 0), Vector2i(20, 20))
+	var paths: Array = []
+	root_node.split(3, paths)
+
+	var floor_arr: Array[Vector2i] = []
+	for leaf in root_node.get_leaves():
+		var padding = Vector4i.ZERO
+		for x in range(leaf.size.x):
+			for y in range(leaf.size.y):
+				if not is_inside_padding(x, y, leaf, padding):
+					var curr_pos = Vector2i(x + leaf.position.x, y + leaf.position.y)
+					floor_arr.append(curr_pos)
+
+	for path in paths:
+		if path['left'].y == path['right'].y:
+			for i in range(path['right'].x - path['left'].x):
+				var curr_pos = Vector2i(path['left'].x+i,path['left'].y)
+				floor_arr.append(curr_pos)
+		else:
+			for i in range(path['right'].y - path['left'].y):
+				var curr_pos = Vector2i(path['left'].x,path['left'].y+i)
+				floor_arr.append(curr_pos)
+	return floor_arr
+
+
+func is_inside_padding(x, y, leaf, padding):
+	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
--- godot/procedural/proc_gen_world.gd
+++ godot/procedural/proc_gen_world.gd
@@ -3,52 +3,31 @@ extends Node2D
 
 
 const WORLD_TILE_SET = 0
-const DUNGEON_GROUND_TILE = Vector2i(8, 0)
+const DUNGEON_GROUND_TILE = Vector2i(7, 0)
 
-var root_node: Branch
 var tile_size: int = 32
-var paths: Array = []
+var dungeon_generator = DungeonGenerator.new()
 
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 
 
 func _ready() -> void:
-	root_node  = Branch.new(Vector2i(0, 0), Vector2i(20, 20))
-	root_node.split(3, paths)
+	var floor_arr = dungeon_generator.generate_dungeon()
 
-	for leaf in root_node.get_leaves():
-		var padding = Vector4i.ZERO
-		for x in range(leaf.size.x):
-			for y in range(leaf.size.y):
-				if not is_inside_padding(x, y, leaf, padding):
-					var curr_pos = Vector2i(x + leaf.position.x, y + leaf.position.y)
-					tile_map_layer.set_cell(curr_pos, WORLD_TILE_SET, DUNGEON_GROUND_TILE)
-
-	for path in paths:
-		if path['left'].y == path['right'].y:
-			for i in range(path['right'].x - path['left'].x):
-				var curr_pos = Vector2i(path['left'].x+i,path['left'].y)
-				tile_map_layer.set_cell(curr_pos, WORLD_TILE_SET, DUNGEON_GROUND_TILE)
-		else:
-			for i in range(path['right'].y - path['left'].y):
-				var curr_pos = Vector2i(path['left'].x,path['left'].y+i)
-				tile_map_layer.set_cell(curr_pos, WORLD_TILE_SET, DUNGEON_GROUND_TILE)
-	queue_redraw()
+	for floor_pos in floor_arr:
+		tile_map_layer.set_cell(floor_pos, WORLD_TILE_SET, DUNGEON_GROUND_TILE)
 
 
-func is_inside_padding(x, y, leaf, padding):
-	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
 
-
-func _draw() -> void:
-	for leaf in root_node.get_leaves():
-		draw_rect(
-			Rect2(
-				leaf.position.x * tile_size,
-				leaf.position.y * tile_size,
-				leaf.size.x * tile_size,
-				leaf.size.y * tile_size
-			), 
-			Color.GREEN,
-			false
-		)
+#func _draw() -> void:
+	#for leaf in root_node.get_leaves():
+		#draw_rect(
+			#Rect2(
+				#leaf.position.x * tile_size,
+				#leaf.position.y * tile_size,
+				#leaf.size.x * tile_size,
+				#leaf.size.y * tile_size
+			#), 
+			#Color.GREEN,
+			#false
+		#)
