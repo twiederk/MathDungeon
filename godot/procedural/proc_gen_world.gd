1ff5c3243aaa3b8904eae040bb2fb4d7ad594d99
in57kc <torsten.wiederkehr@ing.com>
2026-09-15 05:04:05 +0200

    ProcGenWorld inherits from Main

--- godot/procedural/proc_gen_world.gd
+++ godot/procedural/proc_gen_world.gd
@@ -1,5 +1,5 @@
 class_name ProcGenWorld
-extends Node2D
+extends Main
 
 
 const WORLD_TILE_SET: int = 0
@@ -8,8 +8,6 @@ const DUNGEON_WALL_TILE: Vector2i = Vector2i(6, 0)
 
 var dungeon_generator: DungeonGenerator = DungeonGenerator.new()
 
-@onready var tile_map_layer: TileMapLayer = $TileMapLayer
-
 
 func _ready() -> void:
 	var dungeon = dungeon_generator.generate_dungeon(Vector2i(20, 20))
@@ -17,3 +15,4 @@ func _ready() -> void:
 		tile_map_layer.set_cell(floor_pos, WORLD_TILE_SET, DUNGEON_GROUND_TILE)
 	for wall_pos in dungeon.wall_arr:
 		tile_map_layer.set_cell(wall_pos, WORLD_TILE_SET, DUNGEON_WALL_TILE)
+	super._ready()
