6d58de4a95a3e9b61df51e69f72f807455ba013f
in57kc <torsten.wiederkehr@ing.com>
2026-09-16 05:29:44 +0200

    Start implementation of placing enemies in rooms

--- godot/procedural/dungeon.gd
+++ godot/procedural/dungeon.gd
@@ -4,11 +4,12 @@ var root_node: Branch
 var entrance: Vector2i
 var floor_arr: Array[Vector2i] = []
 var wall_arr: Array[Vector2i] = []
+var enemies: Array[Enemy] = []
 
 
-func _init(a_root_node: Branch, a_entrance: Vector2i, a_floor_arr: Array[Vector2i], a_wall_arr: Array[Vector2i]):
+func _init(a_root_node: Branch, a_entrance: Vector2i, a_floor_arr: Array[Vector2i], a_wall_arr: Array[Vector2i], a_enemies: Array[Enemy]):
 	root_node = a_root_node
 	entrance = a_entrance
 	floor_arr = a_floor_arr
 	wall_arr = a_wall_arr
-	
+	enemies = a_enemies
--- godot/procedural/dungeon_generator.gd
+++ godot/procedural/dungeon_generator.gd
@@ -10,13 +10,14 @@ func generate_dungeon(size: Vector2i) -> Dungeon:
 	root_node.split(3, paths)
 	var floor_arr = _place_rooms(root_node)
 	_place_paths(paths, floor_arr)
+	var enemies = _place_enemies(root_node)
 	var entrance = _place_entrance(floor_arr)
 	var wall_arr = _place_walls(size, floor_arr)
-	return Dungeon.new(root_node, entrance, floor_arr, wall_arr)
+	return Dungeon.new(root_node, entrance, floor_arr, wall_arr, enemies)
 
 
 func _place_rooms(root_node: Branch) -> Array[Vector2i]:
-	var floor_arr: Array[Vector2i]
+	var floor_arr: Array[Vector2i] = []
 	for leaf in root_node.get_leaves():
 		var padding = Vector4i.ZERO
 		for x in range(leaf.size.x):
@@ -45,7 +46,7 @@ func _place_entrance(floor_arr: Array[Vector2i]) -> Vector2i:
 
 
 func _place_walls(size: Vector2i, floor_arr: Array[Vector2i]) -> Array[Vector2i]:
-	var wall_arr: Array[Vector2i]
+	var wall_arr: Array[Vector2i] = []
 	for x in range(size.x + 1):
 		for y in range(size.y + 1):
 			var curr_pos = Vector2i(x, y)
@@ -56,3 +57,8 @@ func _place_walls(size: Vector2i, floor_arr: Array[Vector2i]) -> Array[Vector2i]
 
 func is_inside_padding(x, y, leaf, padding) -> bool:
 	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
+
+
+func _place_enemies(root_node: Branch) -> Array[Enemy]:
+	var enemies: Array[Enemy] = []
+	return enemies
