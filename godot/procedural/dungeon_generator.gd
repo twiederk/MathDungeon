490b5d9a81a66d512f80c5126494b3a88fc8cae5
in57kc <torsten.wiederkehr@ing.com>
2026-09-17 03:10:30 +0200

    Place enemies in rooms

--- godot/procedural/dungeon_generator.gd
+++ godot/procedural/dungeon_generator.gd
@@ -1,7 +1,23 @@
 class_name DungeonGenerator
 
 
+# Enemies
+const ZOMBIE_BABY_SCENE = preload("res://enemies/zombie_baby.tscn")
+const ZOMBIE_SCENE = preload("res://enemies/zombie.tscn")
+const DROWN_SCENE = preload("res://enemies/drown.tscn")
+const CREEPER_SCENE = preload("res://enemies/creeper.tscn")
+const SPIDER_SCENE = preload("res://enemies/spider.tscn")
+const SHOOTING_SKELETON_SCENE = preload("res://enemies/shooting_skeleton.tscn")
+const PIGLIN_SCENE = preload("res://enemies/piglin.tscn")
+const PIGLIN_ZOMBIE_SCENE = preload("res://enemies/piglin_zombie.tscn")
 const ENDERMAN_SCENE = preload("res://enemies/enderman.tscn")
+
+# Items
+const WOOD_HELMET_SCENE = preload("res://items/helmet_wood.tscn")
+const STONE_HELMET_SCENE = preload("res://items/helmet_stone.tscn")
+const STONE_SWORD_SCENE = preload("res://items/sword_stone.tscn")
+const IRON_SWORD_SCENE = preload("res://items/sword_iron.tscn")
+const HEALING_POTION_SCENE = preload("res://items/healing_potion.tscn")
 const EYE_OF_ENDER_SCENE = preload("res://items/eye_of_ender.tscn")
 
 
@@ -66,8 +82,29 @@ func is_inside_padding(x, y, leaf, padding) -> bool:
 
 func _place_enemies(root_node: Branch) -> Array[Enemy]:
 	var enemies: Array[Enemy] = []
-	var last_room = root_node.get_leaves()[-1]
+	var enemies_easy = [ZOMBIE_BABY_SCENE, ZOMBIE_SCENE, DROWN_SCENE]
+	var enemies_medium = [CREEPER_SCENE, SPIDER_SCENE, SHOOTING_SKELETON_SCENE, PIGLIN_SCENE, PIGLIN_ZOMBIE_SCENE]
 
+	var rooms = root_node.get_leaves()
+	var number_of_rooms = rooms.size()
+	@warning_ignore("integer_division")
+	var half_number_of_rooms: int = number_of_rooms / 2
+
+	for room_index in range(1, half_number_of_rooms + 1):
+		var room = rooms[room_index]
+		var enemy_scene = enemies_easy.pick_random()
+		var enemy = enemy_scene.instantiate() as Enemy
+		enemy.position = room.get_center()
+		enemies.append(enemy)
+
+	for room_index in range(half_number_of_rooms + 1, number_of_rooms - 1):
+		var room = rooms[room_index]
+		var enemy_scene = enemies_medium.pick_random()
+		var enemy = enemy_scene.instantiate() as Enemy
+		enemy.position = room.get_center()
+		enemies.append(enemy)
+
+	var last_room = rooms[-1]
 	var enderman = ENDERMAN_SCENE.instantiate() as Enemy
 	enderman.position = last_room.get_center()
 	enemies.append(enderman)
