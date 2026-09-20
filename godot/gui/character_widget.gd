210314e368002848c78113bbc5fbe5bc31b8c43d
in57kc <torsten.wiederkehr@ing.com>
2026-09-20 12:22:52 +0200

    Added display of number of wolves

--- godot/gui/character_widget.gd
+++ godot/gui/character_widget.gd
@@ -5,18 +5,21 @@ extends Control
 @onready var health_meter_widget: HealthMeterWidget = $VBoxContainer/HealthMeterWidget
 @onready var damage_label: Label = $VBoxContainer/DamageLabel
 @onready var armor_label: Label = $VBoxContainer/ArmorLabel
+@onready var companion_label = $VBoxContainer/CompanionLabel
 
 
 func _ready() -> void:
-	_update_stats(PlayerStats.hit_points, PlayerStats.max_hit_points, PlayerStats.get_total_damage(), PlayerStats.armor)
-
+	update_stats()
 
 func update_stats() -> void:
-	_update_stats(PlayerStats.hit_points, PlayerStats.max_hit_points, PlayerStats.get_total_damage(), PlayerStats.armor)
+	var hit_points = PlayerStats.hit_points
+	var max_hit_points = PlayerStats.max_hit_points
+	var damage = PlayerStats.get_total_damage()
+	var armor = PlayerStats.armor
+	var number_of_companions = PlayerStats.companion_paths.size()
 
-
-func _update_stats(hit_points: int, max_hit_points: int, damage: int, armor: int) -> void:
 	health_meter_widget.update_health_ui(hit_points)
 	health_meter_widget.update_max_health_ui(max_hit_points)
 	damage_label.text = "Schaden: " + str(damage)
 	armor_label.text = "Rüstung: " + str(armor)
+	companion_label.text = "Wölfe: " + str(number_of_companions)
